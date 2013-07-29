1b033b9523029ad804513362a2caad13bb5b43c7
Kyle Colantonio <kyle10468@gmail.com>
2013-07-29 02:25:29 -0400

    Removed Redundant Files

--- 0Compile.bat
+++
@@ -1,4 +0,0 @@
-@ECHO OFF
-javac -classpath ./lib/pircbotx-1.9.jar;./lib/javax.mail.jar;./lib/json.jar;./lib/jsoup.jar;./lib/chatbot.jar;./lib/apache-commons.jar ./com/iarekylew00t/ircbot/*.java ./com/iarekylew00t/ircbot/handlers/*.java ./com/iarekylew00t/ircbot/listeners/*.java ./com/iarekylew00t/email/*.java ./com/iarekylew00t/google/*.java ./com/iarekylew00t/helpers/*.java ./com/iarekylew00t/chatbot/*.java ./com/iarekylew00t/managers/*.java ./com/iarekylew00t/encryption/*.java
-ECHO ----- ALL FILES COMPILED SUCCESSFULLY -----
-PAUSE
--- 1Run.bat
+++
@@ -1,3 +0,0 @@
-@ECHO OFF
-java -Dfile.encoding=UTF8 -d64 -server -classpath .;./lib/pircbotx-1.9.jar;./lib/javax.mail.jar;./lib/json.jar;./lib/jsoup.jar;./lib/chatbot.jar;./lib/apache-commons.jar com.iarekylew00t.ircbot.IRCBotMain -XX:UseParallelOldGC -Xms1024M - Xmx1024M -XX:NewRatio=3 
-pause
